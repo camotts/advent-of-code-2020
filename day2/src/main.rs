af5a43769558328f9b2d6523cf8fe5d3b70e1eaa
Cameron Otts <otts.cameron@gmail.com>
2020-12-02 10:08:00 -0600

    added problem 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,6 +4,10 @@ use std::path::Path;
 use regex::Regex;
 
 fn main() {
+    problem2();
+}
+
+fn problem1() {
     let re = Regex::new("^(\\d*)-(\\d*) (.): (.*)$").unwrap();
     let mut valid_ct = 0;
     if let Ok(lines) = read("./input") {
@@ -17,6 +21,36 @@ fn main() {
                 let ct = word.matches(target).count();
                 if ct <= max && ct >= min {
                     valid_ct = valid_ct + 1;
+                }
+            }
+        }
+    }
+    println!("{}", valid_ct);
+}
+
+fn problem2() {
+    let re = Regex::new("^(\\d*)-(\\d*) (.): (.*)$").unwrap();
+    let mut valid_ct = 0;
+    if let Ok(lines) = read("./input") {
+        for line in lines {
+            if let Ok(ip) = line {
+                let caps = re.captures(&ip).unwrap();
+                let min = caps.get(1).map_or(1000000, |m| m.as_str().parse().unwrap())-1;
+                let max = caps.get(2).map_or(0, |m| m.as_str().parse().unwrap())-1;
+                let target = caps.get(3).map_or('\0', |m| m.as_str().chars().next().unwrap());
+                let word = caps.get(4).map_or("", |m| m.as_str());
+                let mut first = '\0';
+                for (i, c) in word.chars().enumerate() {
+                    if i == min || i == max {
+                        if first == '\0' {
+                            first = c
+                        } else {
+                            if first != c && (first == target || c == target) {
+                                valid_ct = valid_ct + 1
+                            }
+                        }
+                    }
+
                 }
             }
         }
